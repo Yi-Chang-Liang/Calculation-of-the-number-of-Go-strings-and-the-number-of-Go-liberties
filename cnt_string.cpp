0f33cdc868824d939e1b36d026acb271afa18fd8
Yi-Chang-Liang <61614567+Yi-Chang-Liang@users.noreply.github.com>
2022-06-03 19:12:18 +0800

    Create cnt_string.cpp

---
+++ cnt_string.cpp
@@ -0,0 +1,179 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <time.h>
+#include <iomanip>
+#define edge 9 
+using namespace std;
+
+class GO
+{
+    public:
+        void input();
+        void output();
+        void initialization();//初始化//
+        void visit(int x, int y);
+        void add_path(int x, int y);//將已拜訪點加入path中//
+        int count();//計算棋串長度//
+        int path_back_x();//取得path中某一點的x座標//
+        int path_back_y();//取得path中某一點的y座標//
+        void behavior(int x, int y, int color);
+        void execut();
+    private:
+        int go[edge][edge];
+        int ans[edge][edge];
+        bool gocnt[edge][edge];//紀錄棋子是否已拜訪//
+        vector<int> path_x;//紀錄search path//
+        vector<int> path_y;//紀錄search path//
+};
+
+void GO::input()
+{
+    ifstream gofile;
+    gofile.open("goinput.txt");
+
+    for (int i = 0; i < edge; i++)
+    {
+        for (int j = 0; j < edge; j++)
+            gofile >> go[i][j];
+    }
+
+    gofile.close();
+}
+
+void GO::output()
+{
+   ofstream ansfile;
+    ansfile.open("gooutput_Q.txt");
+    for (int i = 0; i < edge; i++)
+    {
+        for (int j = 0; j < edge; j++)
+        {
+            if(j != edge - 1)
+                ansfile << setw(5) << ans[i][j] << " ";
+            else
+                ansfile << setw(5) << ans[i][j] << endl;
+
+        }
+    }
+}
+
+void GO::initialization()
+{
+    for (int i = 0; i < edge; i++)
+    {
+        for (int j = 0; j < edge; j++) 
+        {
+            gocnt[i][j] = false;
+            ans[i][j] = 0;
+        }
+    }
+}
+
+void GO::visit(int x, int y)
+{
+    gocnt[x][y] = true;
+}
+
+void GO::add_path(int x, int y)
+{
+    path_x.push_back(x);
+    path_y.push_back(y);
+}
+
+int GO::count()
+{
+    int n;
+    n = path_x.size();
+    return n;
+}
+
+int GO::path_back_x()
+{
+    int x;
+    x = path_x.back();
+    path_x.pop_back();
+    return x;
+}
+
+int GO::path_back_y()
+{
+    int y;
+    y = path_y.back();
+    path_y.pop_back();
+    return y;
+}
+
+void GO::behavior(int x, int y, int color)
+{
+	visit(x,y);//將點(x,y)設為已拜訪//
+    add_path(x, y);//將點加入path//
+    if((x + 1) < edge)//檢查點(x+1,y)是否合法//
+    {    
+        if(go[x + 1][y] == color && gocnt[x + 1][y] == false)//該點有棋子且與自己同色且尚未拜訪//
+            behavior(x + 1, y, go[x][y]);
+    }
+
+    if((x - 1) >= 0)
+    {    
+        if(go[x - 1][y] == color && gocnt[x - 1][y] == false)
+            behavior(x - 1, y, go[x][y]);
+    }
+   
+    if((y + 1) < edge)
+    {    
+        if(go[x][y + 1] == color && gocnt[x][y + 1] == false)
+            behavior(x, y + 1, go[x][y]);
+    }
+
+    if((y - 1) >= 0)
+    {    
+        if(go[x][y - 1] == color && gocnt[x][y - 1] == false)
+            behavior(x, y - 1, go[x][y]);
+    }
+}
+
+void GO::execut()
+{
+    for (int i = 0; i < edge; i++)
+    {
+        for (int j = 0; j < edge; j++)
+        {
+            if(gocnt[i][j] == true || go[i][j] == 0)//該點已拜訪或為空格//
+                continue;
+            else
+            {
+                behavior(i, j, go[i][j]);
+                int n;
+                n = count();//取得棋串長度//
+                while(!path_x.empty())//將答案寫入矩陣//
+                {
+                    int x, y;
+                    x = path_back_x();
+                    y = path_back_y();
+                    ans[x][y] = n;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    long double timestart = 0;
+	long double timeend = 0;
+	long double Executiontime = 0; 
+    timestart = clock();
+    GO go;
+    go.input();
+    go.initialization();
+	go.execut();
+    go.output();
+    timeend = clock();
+	Executiontime = (timeend - timestart) / CLOCKS_PER_SEC;
+	printf("Execution time = %.16Lf Second\n", Executiontime);
+	cout << "Execution Completed!" << endl;
+    system("pause");
+    return 0;
+}
